6ee7ce56c7e8d8951265f979f6c818dc98e681a3
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-14 22:35:44 +0300

    add go git 😅

--- internal/project/project.go
+++ internal/project/project.go
@@ -3,19 +3,42 @@ package project
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/go-git/go-git/v6"
 )
 
 func Root(inputPath string) (string, error) {
-	fileInfo, err := os.Stat(inputPath)
-	if err != nil {
-		return "", err
-	}
 	absolutePath, err := filepath.Abs(inputPath)
 	if err != nil {
 		return "", err
 	}
+
+	if repository, err := getGitRoot(absolutePath); err == nil {
+		return repository, nil
+	}
+
+	fileInfo, err := os.Stat(inputPath)
+	if err != nil {
+		return "", err
+	}
+
 	if !fileInfo.IsDir() {
 		return filepath.Dir(absolutePath), nil
 	}
+
 	return absolutePath, nil
+}
+
+func getGitRoot(path string) (string, error) {
+	repo, err := git.PlainOpenWithOptions(path, &git.PlainOpenOptions{
+		DetectDotGit: true, // detect parent .git directories
+	})
+	if err != nil {
+		return "", err
+	}
+	wt, err := repo.Worktree()
+	if err != nil {
+		return "", err
+	}
+	return wt.Filesystem.Root(), nil
 }
--- internal/project/project_test.go
+++ internal/project/project_test.go
@@ -4,11 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/go-git/go-git/v6"
 )
 
 func TestRootDirectory(t *testing.T) {
 	var dir = t.TempDir()
+
 	root, err := Root(dir)
 
 	if err != nil {
@@ -48,6 +52,27 @@ func TestRootInvalidFile(t *testing.T) {
 	}
 
 	if got, want := err.Error(), fmt.Sprintf("stat %s: no such file or directory", file); got != want {
+		t.Fatalf("got=%v, want=%v", got, want)
+	}
+}
+
+func TestRootRepository(t *testing.T) {
+	repository := t.TempDir()
+	git.PlainInit(repository, false)
+
+	dir := filepath.Join(repository, "path/to/dir/in/repository")
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		t.Fatalf("err=%v", err)
+	}
+
+	root, err := Root(dir)
+
+	if err != nil {
+		t.Fatalf("err=%v", err)
+	}
+
+	if got, want := root, repository; got != want {
 		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
