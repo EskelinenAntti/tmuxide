e993205aa2959d5f4b5c334bdf807b031fe8aba4
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-14 21:49:34 +0300

    Improve tests

--- internal/project/project_test.go
+++ internal/project/project_test.go
@@ -1,35 +1,53 @@
 package project
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"testing"
 )
 
-func TestDirectory(t *testing.T) {
-	var want = t.TempDir()
-	got, err := Root(want)
+func TestRootDirectory(t *testing.T) {
+	var dir = t.TempDir()
+	root, err := Root(dir)
 
 	if err != nil {
-		t.Fatalf("Root(%s): err = %v", want, err)
+		t.Fatalf("err=%v", err)
 	}
 
-	if got != want {
-		t.Fatalf("Root(%s) = %s, want %s", want, got, want)
+	if got, want := root, dir; got != want {
+		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
 
-func TestFile(t *testing.T) {
-	want := t.TempDir()
-	file := want + "/file.txt"
+func TestRootFile(t *testing.T) {
+	dir := t.TempDir()
+	file := dir + "/file.txt"
 	os.WriteFile(file, []byte{}, 0644)
 
-	got, err := Root(file)
+	root, err := Root(file)
 
 	if err != nil {
-		t.Fatalf("Root(%s): err = %v", want, err)
+		t.Fatalf("err=%v", err)
 	}
 
-	if got != want {
-		t.Fatalf("Root(%s) = %s, want %s", file, got, want)
+	if got, want := root, dir; got != want {
+		t.Fatalf("got=%v, want=%v", got, want)
+	}
+}
+
+func TestRootInvalidFile(t *testing.T) {
+	dir := t.TempDir()
+	file := dir + "/does-not-exist.txt"
+
+	_, err := Root(file)
+
+	var pathError *os.PathError
+	if got, want := err, &pathError; !errors.As(got, want) {
+		t.Fatalf("got=%T, want=%T", got, want)
+	}
+
+	if got, want := err.Error(), fmt.Sprintf("stat %s: no such file or directory", file); got != want {
+		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
