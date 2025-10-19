1aeea543a78577437750e60fb86d6f471ef06297
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-19 17:38:55 +0300

    simplify tests

--- internal/ide/project_test.go
+++ internal/ide/project_test.go
@@ -28,43 +28,45 @@ func (git GitMock) RevParse(cwd string) (string, error) {
 	return git.repository, git.err
 }
 
-var shellMock = shell.Shell{
-	Git: GitMock{
-		repository: "",
-		err:        errors.New("not inside git repository"),
-	},
-	Tmux: nil,
-	Path: PathMock{},
+var testEditor string = "editor"
+var errNotInRepository = errors.New("not in repository")
+
+func shellMock(repository string) shell.Shell {
+	var err error
+	if repository == "" {
+		err = errNotInRepository
+	}
+
+	return shell.Shell{
+		Git: GitMock{
+			repository: repository,
+			err:        err,
+		},
+		Tmux: nil,
+		Path: PathMock{},
+	}
 }
 
-var testEditor string = "editor"
-
-func wantWindows(target string) []tmux.WindowCommand {
-	return []tmux.WindowCommand{
+func wantWindows(target string, lazygit bool) []tmux.WindowCommand {
+	windows := []tmux.WindowCommand{
 		{
 			Cmd:  testEditor,
 			Args: []string{target},
 		}}
-}
-
-func wantWindowsInRepository(target string) []tmux.WindowCommand {
-	return []tmux.WindowCommand{
-		{
-			Cmd:  testEditor,
-			Args: []string{target},
-		},
-		{
+	if lazygit {
+		windows = append(windows, tmux.WindowCommand{
 			Cmd:  "lazygit",
 			Args: nil,
-		},
+		})
 	}
+	return windows
 }
 
 func TestProjectForDirectory(t *testing.T) {
 	t.Setenv("EDITOR", testEditor)
 	var dir = t.TempDir()
 
-	project, err := ProjectFor(dir, shellMock)
+	project, err := ProjectFor(dir, shellMock(""))
 
 	if err != nil {
 		t.Fatalf("err=%v", err)
@@ -78,7 +80,7 @@ func TestProjectForDirectory(t *testing.T) {
 		t.Fatalf("%v did not start with %v", got, want)
 	}
 
-	if got, want := project.Windows, wantWindows(dir); !reflect.DeepEqual(got, want) {
+	if got, want := project.Windows, wantWindows(dir, false); !reflect.DeepEqual(got, want) {
 		t.Fatalf("got=%v, want=%v", got, want)
 	}
 
@@ -91,7 +93,7 @@ func TestProjectForFile(t *testing.T) {
 	file := dir + "/file.txt"
 	os.WriteFile(file, []byte{}, 0644)
 
-	project, err := ProjectFor(file, shellMock)
+	project, err := ProjectFor(file, shellMock(""))
 
 	if err != nil {
 		t.Fatalf("err=%v", err)
@@ -105,7 +107,7 @@ func TestProjectForFile(t *testing.T) {
 		t.Fatalf("%v did not start with %v", got, want)
 	}
 
-	if got, want := project.Windows, wantWindows(file); !reflect.DeepEqual(got, want) {
+	if got, want := project.Windows, wantWindows(file, false); !reflect.DeepEqual(got, want) {
 		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
@@ -120,15 +122,7 @@ func TestProjectForDirectoryInRepository(t *testing.T) {
 		t.Fatalf("err=%v", err)
 	}
 
-	shellMock := shell.Shell{
-		Git: GitMock{
-			repository: repository,
-			err:        nil,
-		},
-		Path: PathMock{},
-	}
-
-	project, err := ProjectFor(dir, shellMock)
+	project, err := ProjectFor(dir, shellMock(repository))
 
 	if err != nil {
 		t.Fatalf("err=%v", err)
@@ -142,7 +136,7 @@ func TestProjectForDirectoryInRepository(t *testing.T) {
 		t.Fatalf("%v did not start with %v", got, want)
 	}
 
-	if got, want := project.Windows, wantWindowsInRepository(dir); !reflect.DeepEqual(got, want) {
+	if got, want := project.Windows, wantWindows(dir, true); !reflect.DeepEqual(got, want) {
 		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
@@ -154,15 +148,7 @@ func TestProjectForFileInRepository(t *testing.T) {
 	file := repository + "/file.txt"
 	os.WriteFile(file, []byte{}, 0644)
 
-	shellMock := shell.Shell{
-		Git: GitMock{
-			repository: repository,
-			err:        nil,
-		},
-		Path: PathMock{},
-	}
-
-	project, err := ProjectFor(file, shellMock)
+	project, err := ProjectFor(file, shellMock(repository))
 
 	if err != nil {
 		t.Fatalf("err=%v", err)
@@ -176,7 +162,7 @@ func TestProjectForFileInRepository(t *testing.T) {
 		t.Fatalf("%v did not start with %v", got, want)
 	}
 
-	if got, want := project.Windows, wantWindowsInRepository(file); !reflect.DeepEqual(got, want) {
+	if got, want := project.Windows, wantWindows(file, true); !reflect.DeepEqual(got, want) {
 		t.Fatalf("got=%v, want=%v", got, want)
 	}
 }
@@ -187,7 +173,7 @@ func TestProjectForInvalidFile(t *testing.T) {
 	dir := t.TempDir()
 	file := dir + "/does-not-exist.txt"
 
-	_, err := ProjectFor(file, shellMock)
+	_, err := ProjectFor(file, shellMock(""))
 
 	var pathError *os.PathError
 	if got, want := err, &pathError; !errors.As(got, want) {
